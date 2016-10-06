8f9e9e7bc324ae15988294eddadb34abab6b6550
David LLuna <llunaplanet@gmail.com>
2016-10-06 11:37:41 +0200

    Encapsulate the functionality into a class and make some dirty tests

--- world-temperature.rb
+++ world-temperature.rb
@@ -3,30 +3,52 @@
 require 'net/https'
 require 'json'
 
-# Forecast API Key from https://developer.forecast.io
-forecast_api_key = "99f08919415972d78b757e13a6fc1345"
+class ForecastIO
 
-# Latitude, Longitude for location
-forecast_location_lat = "45.429522"
-forecast_location_long = "-75.689613"
+    attr_accessor :units
+    attr_accessor :api_key
 
-# Unit Format
-# "us" - U.S. Imperial
-# "si" - International System of Units
-# "uk" - SI w. windSpeed in mph
-forecast_units = "si"
+    def initialize(api_key, units = "si")
 
-# SCHEDULER.every '5m', :first_in => 0 do |job|
-  http = Net::HTTP.new("api.forecast.io", 443)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-  api_endpoint = "/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"
-  response = http.request(Net::HTTP::Get.new(api_endpoint))
-  forecast = JSON.parse(response.body)
-  forecast_current_temp = forecast["currently"]["temperature"].round
-  forecast_hour_summary = forecast["minutely"]["summary"]
-  #send_event('forecast', { temperature: "#{forecast_current_temp}&deg;", hour: "#{forecast_hour_summary}"})
-  puts "Current temperature: #{forecast_current_temp}"
-  puts "Forecast summary: #{forecast_hour_summary}"
-  puts forecast
-# end
+        # Unit Format
+        # "us" - U.S. Imperial
+        # "si" - International System of Units
+        # "uk" - SI w. windSpeed in mph
+
+        @api_key = api_key
+        @units = units
+    end
+
+    def get_forecast(latitude, longitude)
+        http = Net::HTTP.new("api.forecast.io", 443)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        api_endpoint = "/forecast/#{@api_key}/#{latitude},#{longitude}?units=#{@units}"
+        response = http.request(Net::HTTP::Get.new(api_endpoint))
+        forecast = JSON.parse(response.body)
+        return forecast
+    end
+
+end
+
+if __FILE__ == $0
+
+    api_key = "99f08919415972d78b757e13a6fc1345"
+    location_lat = "45.429522"
+    location_long = "-75.689613"
+
+    forecastIO = ForecastIO.new(api_key)
+    forecast = forecastIO.get_forecast(location_lat, location_long)
+    forecast_current_temp = forecast["currently"]["temperature"]
+    forecast_hour_summary = forecast["minutely"]["summary"]
+    puts "Current temperature: #{forecast_current_temp}"
+    puts "Text summary: #{forecast_hour_summary}"
+
+    forecastIO.units="us";
+    forecast = forecastIO.get_forecast(location_lat, location_long)
+    forecast_current_temp = forecast["currently"]["temperature"]
+    forecast_hour_summary = forecast["minutely"]["summary"]
+    puts "Current temperature: #{forecast_current_temp}"
+    puts "Text summary: #{forecast_hour_summary}"
+
+end
